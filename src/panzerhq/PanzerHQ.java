a53ce71e6ee11b3c6588af9df91ed59355c33d58
GaudiNicki <33468767+GaudiNicki@users.noreply.github.com>
2018-11-30 21:54:14 +0100

    Add files via upload

---
+++ src/panzerhq/PanzerHQ.java
@@ -0,0 +1,13 @@
+package panzerhq;
+
+public class PanzerHQ {
+
+	public PanzerHQ() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args){
+		new GameWindow();
+	}
+
+}
